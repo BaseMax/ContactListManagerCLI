27cc2a1483ce399990d9d41ebc375dea153fca16
MaxianEdison <MaximilianEdison@gmail.com>
2023-04-24 23:34:55 +0330

    Add support for windows when want clean screen.

--- src/main.c
+++ src/main.c
@@ -3,10 +3,16 @@
 
 int main(void) {
     // Clear terminal screeen (Linux/macOS)
-    if ( system("clear") != 0) {
+#ifdef _WIN32
+    if (system("cls") != 0) {
         printf("Error occurred when clear screen!\n"); 
     }
-    if (puts("Hello world!") == EOF) {
+#else
+    if (system("clear") != 0) {
+        printf("Error occurred when clear screen!\n");
+    }
+#endif
+    if (puts("Contact List Manager") == EOF) {
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
